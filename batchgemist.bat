923be50256b5f9bdae994f29b1f4a3c49c43d8fd
Reino17 <rwijnsma@xs4all.nl>
2018-03-01 17:47:53 +0100

    Update Nickelodeon extractor - Zet container-bitrate in het 'vbitrate'-attribuut (maar uiteraard zonder "v:" vooraf).

--- batchgemist.bat
+++ batchgemist.bat
@@ -2272,7 +2272,7 @@ IF NOT "%url: =%"=="%url%" (
 	          'x'^,
 	          @height
 	        ^)^,
-	        'bitrate':@bitrate^|^|'k'^,
+	        'vbitrate':@bitrate^|^|'k'^,
 	        'url':src
 	      }
 	    ]^" --output-encoding^=oem --output-format^=cmd^"') DO %%A
